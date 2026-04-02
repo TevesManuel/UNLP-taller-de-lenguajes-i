db988039bee8411576e99c9170ba0e0222de9ba7
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2026-04-02 13:19:28 -0300

    1.15)

---
+++ Practica-1/15/app.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int iterativeFactorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    printf("%d\n", factorial(5));
+    printf("%d\n", iterativeFactorial(5));
+    return 0;
+}
